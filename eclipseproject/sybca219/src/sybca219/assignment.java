02e5f313bcf425896cff40773042f0198d1c8007
nishimay <nishiparodkar@gmail.com>
2024-01-13 10:46:28 +0530

    a1

---
+++ eclipseproject/sybca219/src/sybca219/assignment.java
@@ -0,0 +1,14 @@
+package sybca219;
+
+public class assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n1=9,n2=5,sum;
+		System.out.println("program to add 2 nos");
+		sum=n1+n2;
+
+	}
+
+}
